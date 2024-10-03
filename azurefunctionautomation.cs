743958d36fa822e90caefaa0fb56b9e491a57571
mahad nadeem <mahad.nadeem@dotfoods.com>
2024-10-03 15:43:20 +0500

    createdddd azure function

---
+++ azurefunctionautomation.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class azurefunctionautomation
+    {
+        private readonly ILogger<azurefunctionautomation> _logger;
+
+        public azurefunctionautomation(ILogger<azurefunctionautomation> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("azurefunctionautomation")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions! mahad ");
+        }
+    }
+}
